92e8521f9200e53a414ffa66e96ed7d859f1ef72
Nick Edwards <nickedwrds@gmail.com>
2018-11-15 10:13:47 +0000

    Alternative setup where letsencrypt is run from the container before starting apache to get a certificate and install it in apache. The renewals .conf is then hacked to change the renewal method to apache. In theory renewals should then happen automagically via cron set up by the certbot package - not testeded! Had to add a VirtualHost on port 80 as certbot needs this

---
+++ docker/webserver/init_letsencrypt.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Init Letsencrypt $LETSENCRYPT_HOME  $DOMAINS $STAGING"
+
+# init only if lets-encrypt is running for the first time and if DOMAINS was set
+if ([ ! -d $LETSENCRYPT_HOME/live ] || [ ! "$(ls -A $LETSENCRYPT_HOME)" ]) && [ ! -z "$DOMAINS" ]; then
+	if (! [ -z "$STAGING" ]) then
+	  echo "Using Let's Encrypt Staging environment..."
+	  certbot -n --staging --expand --authenticator standalone --installer apache --agree-tos --email $WEBMASTER_MAIL --domains $DOMAINS
+	else
+	  echo "Using Let's Encrypt Production environment..."
+	  certbot -n --expand --authenticator standalone --installer apache --agree-tos --email $WEBMASTER_MAIL --domains $DOMAINS
+	fi
+	sed -i .bak 's/standalone/apache/' /etc/letsencrypt/renewals/*.conf
+fi
