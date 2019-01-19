6b5a579029c5706b10bab197fc6caac2ad15c705
Nick Edwards <nickedwrds@gmail.com>
2019-01-19 23:14:41 +0000

    Hour and minutes were the wrong way round in crontab. Fixed issue where init_webserver didn't follow the STAGING variable

--- docker/webserver/init_letsencrypt.sh
+++ docker/webserver/init_letsencrypt.sh
@@ -4,7 +4,7 @@ echo "Init Letsencrypt $LETSENCRYPT_HOME  $DOMAINS $STAGING"
 
 # init only if lets-encrypt is running for the first time and if DOMAINS was set
 if ([ ! -d $LETSENCRYPT_HOME/live ] || [ ! "$(ls -A $LETSENCRYPT_HOME)" ]) && [ ! -z "$DOMAINS" ]; then
-	if (! [ -z "$STAGING" ]) then
+	if (! [ -z "$STAGING" ] ) && [ "$STAGING" != "0"]; then
 	  echo "Using Let's Encrypt Staging environment..."
 	  certbot -n --staging --expand --authenticator standalone --installer apache --agree-tos --email $WEBMASTER_MAIL --domains $DOMAINS
 	else
