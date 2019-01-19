a64d5b0430b46eabf841805450fbd6c925acb900
Nick Edwards <nickedwrds@gmail.com>
2019-01-19 16:46:21 +1300

    Add utf8 conversion to convert script

--- install/clean_dumps/convert.sh
+++ install/clean_dumps/convert.sh
@@ -4,4 +4,8 @@ do
    echo $db
    cp ctcweb9_${db}.sql ${db}.sql
    LC_ALL=C sed -i .bak 's/ctcweb9_//g' ${db}.sql
+   LC_ALL=C sed -i .bak 's/utf8/utf8mb4/g' ${db}.sql
+   LC_ALL=C sed -i .bak 's/utf8_unicode_ci/utf8mb4_unicode_ci/g' ${db}.sql
+   LC_ALL=C sed -i .bak 's/latin1_swedish_ci/utf8mb4_unicode_ci/g' ${db}.sql
+   LC_ALL=C sed -i .bak 's/latin1/utf8mb4/g' ${db}.sql
 done
