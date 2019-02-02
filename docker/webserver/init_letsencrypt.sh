74621ca94e94136e44fef61469adf36bdb8471a4
Nick Edwards <nickedwrds@gmail.com>
2019-02-02 01:02:19 +0000

    Mount the letsencrupt folders as volumes so we dont need to get a new cert every time the container is started

--- docker/webserver/init_letsencrypt.sh
+++ docker/webserver/init_letsencrypt.sh
@@ -2,14 +2,16 @@
 
 echo "Init Letsencrypt $LETSENCRYPT_HOME  $DOMAINS $STAGING"
 
-# init only if lets-encrypt is running for the first time and if DOMAINS was set
-if ([ ! -d $LETSENCRYPT_HOME/live ] || [ ! "$(ls -A $LETSENCRYPT_HOME)" ]) && [ ! -z "$DOMAINS" ]; then
-	if (! [ -z "$STAGING" ] ) && [ "$STAGING" != "0"]; then
+# Initialise letsencypt
+# The certbot command will attempt to obtain a certificate if a valid one is not already present
+# The command also installs the certificate by modifying the apache conf
+if [ ! -z "$DOMAINS" ]; then
+	if (! [ -z "$STAGING" ] ) && [ "$STAGING" != "0" ]; then
 	  echo "Using Let's Encrypt Staging environment..."
 	  certbot -n --staging --expand --authenticator standalone --installer apache --agree-tos --email $WEBMASTER_MAIL --domains $DOMAINS
 	else
 	  echo "Using Let's Encrypt Production environment..."
 	  certbot -n --expand --authenticator standalone --installer apache --agree-tos --email $WEBMASTER_MAIL --domains $DOMAINS
 	fi
-	sed -i .bak 's/standalone/apache/' /etc/letsencrypt/renewals/*.conf
+	sed -i 's/standalone/apache/' /etc/letsencrypt/renewal/*.conf
 fi
