b04218d47adce254a8d1db6b7dbdcc175d707301
Nick Edwards <nickedwrds@gmail.com>
2024-10-30 09:28:38 +0000

    Remove the certbot install step as it seemed to fail a lot, and instead just add the cert manually to the apache config

--- docker/webserver/init_letsencrypt.sh
+++ docker/webserver/init_letsencrypt.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "Init Letsencrypt $LETSENCRYPT_HOME  $DOMAINS $STAGING"
+echo ">>> Init Letsencrypt $LETSENCRYPT_HOME  $DOMAINS $STAGING"
 
 # Initialise letsencypt
 # The certbot command will attempt to obtain a certificate if a valid one is not already present
 # The command also installs the certificate by modifying the apache conf
 if [ ! -z "$DOMAINS" ]; then
 	if (! [ -z "$STAGING" ] ) && [ "$STAGING" != "0" ]; then
-	  echo "Using Let's Encrypt Staging environment..."
+	echo "Using Let's Encrypt Staging environment..."
 	  certbot -n --staging --expand --authenticator standalone --installer apache --agree-tos --email $WEBMASTER_MAIL --domains $DOMAINS
 	else
 	  echo "Using Let's Encrypt Production environment..."
-	  certbot -n --expand --authenticator standalone --installer apache --agree-tos --email $WEBMASTER_MAIL --domains $DOMAINS
+	  certbot certonly -n --expand --authenticator standalone --installer apache --agree-tos --email $WEBMASTER_MAIL --domains $DOMAINS
 	fi
 	sed -i 's/standalone/apache/' /etc/letsencrypt/renewal/*.conf
 fi
